f1c6cbf3d419f64dece008fb320e86e7551e3fa2
Şehran <sehrankartal@gmail.com>
2025-06-30 23:12:43 +0300

    bugfix logging

--- src/main/java/org/example/AdminServiceApplication.java
+++ src/main/java/org/example/AdminServiceApplication.java
@@ -1,12 +1,20 @@
 package org.example;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdminServiceApplication {
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
+
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitUser;
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
     }
@@ -14,5 +22,10 @@ public class AdminServiceApplication {
     @Bean
     public Queue jobQueue() {
         return new Queue("jobQueue", true);
+    }
+    @PostConstruct
+    public void logRabbitMQConfig() {
+        System.out.println(">>>> Spring loaded RabbitMQ Virtual Host: " + virtualHost);
+        System.out.println(">>>> Spring loaded RabbitMQ Username: " + rabbitUser);
     }
 }
