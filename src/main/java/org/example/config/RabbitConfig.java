ece00686c9155dde8ea5cc282ab7ebdedefe5f32
Şehran <sehrankartal@gmail.com>
2025-07-01 15:12:00 +0300

    Rabbit config fixed

--- src/main/java/org/example/config/RabbitConfig.java
+++ src/main/java/org/example/config/RabbitConfig.java
@@ -1,13 +1,30 @@
 package org.example.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
 
     @Value("${SPRING_RABBITMQ_ADDRESS}")
     private String rabbitUri;
